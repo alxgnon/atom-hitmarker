9d5ac6451a6df988dfdb7822338327b2045549d8
Alexandre Gagnon <alxgnon@gmail.com>
2016-12-11 22:54:35 -0500

    remove spec dir

--- spec/atom-hitmarker-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomHitmarkerView from '../lib/atom-hitmarker-view';
-
-describe('AtomHitmarkerView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
