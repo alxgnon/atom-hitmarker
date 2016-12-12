64c9765e0b32011ead289492369162ba9e2eeb49
Alexandre Gagnon <alxgnon@gmail.com>
2016-12-11 22:42:09 -0500

    generate atom package

---
+++ spec/atom-hitmarker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomHitmarkerView from '../lib/atom-hitmarker-view';
+
+describe('AtomHitmarkerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
